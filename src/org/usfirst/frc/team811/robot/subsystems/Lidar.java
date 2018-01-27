11335cbea1fbd96c9ff89480539170a9f0cd6ee1
Meera Kurup <meerakurup19@bghs.org>
2018-01-27 13:27:27 -0500

    Lidar class update method

--- src/org/usfirst/frc/team811/robot/subsystems/Lidar.java
+++ src/org/usfirst/frc/team811/robot/subsystems/Lidar.java
@@ -13,59 +13,71 @@ import edu.wpi.first.wpilibj.SensorBase;
 public class Lidar extends Subsystem{
 
 	private I2C i2c;
-	private static byte[] distance;
 	private java.util.Timer updater;
 	private LIDARUpdater task;
-	
 	private final int LIDAR_ADDR = 0x62;	
 	private final int LIDAR_CONFIG_REGISTER = 0x00;
 	private final int LIDAR_DISTANCE_REGISTER = 0x8f;
+	private final int LIDAR_STATUS_REGISTER = 0x01;
 		
 	public Lidar(Port port) {
 	i2c = new I2C(Port.kMXP, LIDAR_ADDR);
-			
-	distance = new byte[2];
 	 
 	task = new LIDARUpdater();
 	updater = new java.util.Timer();
 	}
 		
 		// Distance in cm
-		public static int getDistance() {
-			return (int)Integer.toUnsignedLong(distance[0] << 8) + Byte.toUnsignedInt(distance[1]);
-		}
-	 
-		public double pidGet() {
-			return getDistance();
-		}
+	public static int getDistance() {
+		return (int)Integer.toUnsignedLong(distance[0] << 8) + Byte.toUnsignedInt(distance[1]);
+	}
+ 
+	public double pidGet() {
+		return getDistance();
+	}
 		
 		// Start 10Hz polling
-		public void start() {
-			updater.scheduleAtFixedRate(task, 0, 20);
-		}
+	public void start() {
+		updater.scheduleAtFixedRate(task, 0, 20);
+	}
 		
 		// Start polling for period in milliseconds
-		public void start(int period) {
-			updater.scheduleAtFixedRate(task, 0, period);
+	public void start(int period) {
+		updater.scheduleAtFixedRate(task, 0, period);
+	}
+		
+	public void stop() {
+		updater.cancel();
+	}
+		
+	// Update distance variable
+	public void update() {
+		
+		byte[] distance = new byte[2];
+		i2c.write(LIDAR_CONFIG_REGISTER, 0x04);
+		int i = 0;
+		byte[] status = new byte[1];
+		
+		i2c.read(LIDAR_STATUS_REGISTER, 1, status);
+		while(status[0] != 0)
+		{
+			i2c.read(LIDAR_STATUS_REGISTER, 1, status);
 		}
 		
-		public void stop() {
-			updater.cancel();
-		}
+		i2c.read(LIDAR_DISTANCE_REGISTER, 1, distance); 
 		
-		// Update distance variable
-		public void update() {
-			i2c.write(LIDAR_CONFIG_REGISTER, 0x04); // Initiate measurement
-			Timer.delay(0.04); // Delay for measurement to be taken
-			i2c.read(LIDAR_DISTANCE_REGISTER, 2, distance); // Read in measurement
-			Timer.delay(0.01); // Delay to prevent over polling
-		}
+		//i2c.read(LIDAR_DISTANCE_REGISTER, 2, distance); // Read in measurement
+		//i2c.write(LIDAR_CONFIG_REGISTER, 0x04);// Initiate measurement
+		//Timer.delay(0.04); // Delay for measurement to be taken
+		//Timer.delay(0.01); // Delay to prevent over polling
+		
+	}
 		
 		// Timer task to keep distance updated
-		private class LIDARUpdater extends TimerTask {
-			public void run() {
-				while(true) {
-					update(); 
+	private class LIDARUpdater extends TimerTask {
+		public void run() {
+			while(true) {
+				update(); 
 //					if(getDistance() < 90 && getDistance() > 84){
 //						SmartDashboard.putBoolean("Correct distance from human feeder", true);
 //					}
@@ -85,16 +97,13 @@ public class Lidar extends Subsystem{
 //					} catch (InterruptedException e) {
 //						e.printStackTrace();
 //					}
-				}
 			}
 		}
+	}
 
 		@Override
-		protected void initDefaultCommand() {
+	protected void initDefaultCommand() {
 			// TODO Auto-generated method stub
-
+			
 		}
 	}
-	
-	
-
